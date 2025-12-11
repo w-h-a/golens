5903f4ea664a923780ef0ce9b1c90fd7edfa02e1
Wes Anderson <wesley.herb.anderson@gmail.com>
2025-12-11 07:26:05 -0800

    feat: introduce v1 api structs

---
+++ api/dto/v1/dto.go
@@ -0,0 +1,16 @@
+package v1
+
+import "io"
+
+type Request struct {
+	Method  string
+	Path    string
+	Headers map[string][]string
+	Body    io.ReadCloser
+}
+
+type Response struct {
+	StatusCode int
+	Headers    map[string][]string
+	Body       io.Reader
+}
