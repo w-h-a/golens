02c3c3c12a141b41ac1a587f22f04c2dd2ee6c86
Wes Anderson <wesley.herb.anderson@gmail.com>
2025-12-13 17:58:52 -0800

    feat: update v1 event

--- api/event/v1/event.go
+++ api/event/v1/event.go
@@ -6,14 +6,15 @@ import (
 )
 
 type Event struct {
-	Id         string          `json:"id,omitempty" db:"id"`
-	TraceId    string          `json:"trace_id" db:"trace_id"`
-	StartTime  time.Time       `json:"start_time" db:"start_time"`
-	EndTime    time.Time       `json:"end_time" db:"end_time"`
-	DurationMs int64           `json:"duration_ms" db:"duration_ms"`
-	StatusCode int             `json:"status_code" db:"status"`
-	TokenCount int             `json:"token_count" db:"token_count"`
-	Model      string          `json:"model" db:"model"`
-	Request    json.RawMessage `json:"request,omitempty" db:"request"`
-	Response   string          `json:"response,omitempty" db:"response"`
+	Id         string            `json:"id,omitempty" db:"id"`
+	TraceId    string            `json:"trace_id" db:"trace_id"`
+	StartTime  time.Time         `json:"start_time" db:"start_time"`
+	EndTime    time.Time         `json:"end_time" db:"end_time"`
+	DurationMs int64             `json:"duration_ms" db:"duration_ms"`
+	StatusCode int               `json:"status_code" db:"status_code"`
+	TokenCount int               `json:"token_count" db:"token_count"`
+	Model      string            `json:"model" db:"model"`
+	Request    json.RawMessage   `json:"request,omitempty" db:"request"`
+	Response   string            `json:"response,omitempty" db:"response"`
+	Attributes map[string]string `json:"attributes,omitempty" db:"attributes"`
 }
